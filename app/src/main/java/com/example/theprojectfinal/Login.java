f9b75da40576ca1bc969c2ee13d03b20f956578b
Lenovo <allouse2003@gmail.com>
2023-05-09 18:55:54 +0300

    Project

---
+++ app/src/main/java/com/example/theprojectfinal/Login.java
@@ -0,0 +1,27 @@
+package com.example.theprojectfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+      TextView txSignUp = findViewById(R.id.txSignup1);
+
+      txSignUp.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              Intent intent = new Intent(Login.this , signup.class);
+              startActivity(intent);
+          }
+      });
+
+    }
+}
