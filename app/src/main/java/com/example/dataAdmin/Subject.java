01064728a390dd353270c0cc7a43a3a7573352ea
Lenovo <allouse2003@gmail.com>
2023-05-22 14:37:04 +0300

    Project

---
+++ app/src/main/java/com/example/dataAdmin/Subject.java
@@ -0,0 +1,34 @@
+package com.example.dataAdmin;
+
+public class Subject {
+    private int id ;
+    private String name ;
+
+    public static final String TABLE_NAME = "student";
+    public static final String COL_ID = "id";
+    public static final String COL_NAME = "name";
+
+    public static final String CREATE_TABLE = "CREATE CREATE TABLE IF NOT EXISTS "+TABLE_NAME +
+            " ("+COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT ,"+COL_NAME + " TEXT";
+
+    public Subject(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
