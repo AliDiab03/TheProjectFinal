e428ee078ef1a77303a8b72a0418816f73c33d79
Lenovo <allouse2003@gmail.com>
2023-05-28 11:11:16 +0300

    Project

--- app/src/main/java/com/example/Adapter/Adapter_Month.java
+++ app/src/main/java/com/example/Adapter/Adapter_Month.java
@@ -5,8 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.theprojectfinal.Month;
@@ -15,9 +17,11 @@ import com.example.theprojectfinal.R;
 import java.util.ArrayList;
 
 public class Adapter_Month extends RecyclerView.Adapter<Adapter_Month.myHolder> {
-    private String[] months;
+    Context context ;
+     String[] months;
 
-    public Adapter_Month(String[] months) {
+    public Adapter_Month(Context context, String[] months) {
+        this.context = context;
         this.months = months;
     }
 
@@ -25,12 +29,19 @@ public class Adapter_Month extends RecyclerView.Adapter<Adapter_Month.myHolder> 
     @Override
     public myHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.iteam_activity_month,parent,false);
-        return null;
+        return new myHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull myHolder holder, int position) {
         String month = months[position];
+        holder.monthTextView.setText(month);
+        holder.cardMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Clicked to" +month, Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
@@ -41,9 +52,11 @@ public class Adapter_Month extends RecyclerView.Adapter<Adapter_Month.myHolder> 
 
     public  class  myHolder extends RecyclerView.ViewHolder{
         TextView monthTextView;
+        CardView cardMonth ;
         public myHolder(@NonNull View itemView) {
             super(itemView);
             monthTextView = itemView.findViewById(R.id.txMonth);
+          cardMonth =  itemView.findViewById(R.id.cardMonth);
         }
     }
 }
--- app/src/main/java/com/example/theprojectfinal/Month.java
+++ app/src/main/java/com/example/theprojectfinal/Month.java
@@ -11,11 +11,15 @@ import android.widget.TextView;
 import com.example.Adapter.Adapter_Month;
 
 public class Month extends AppCompatActivity {
+    RecyclerView rvMonth ;
+    String months[] = {"يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_month);
+        rvMonth =findViewById(R.id.rvMonth);
 
         Intent intent = getIntent();
         String subjectName = intent.getStringExtra("subjectName");
@@ -24,6 +28,12 @@ public class Month extends AppCompatActivity {
 
         TextView textViewName = findViewById(R.id.txTitleSubject);
         textViewName.setText(subjectName);
+
+Adapter_Month adapter_month = new Adapter_Month(this,months);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        rvMonth.setLayoutManager(layoutManager);
+        rvMonth.setAdapter(adapter_month);
 
 
 
